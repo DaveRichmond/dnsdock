a96227490a8c8e2b08de6475f7512040d6f8459a
David Richmond <dave@prstat.org>
2019-08-23 15:57:34 +1000

    moved more stuff into the deploy step and hopefully fixes issues

--- .travis/post-build.sh
+++ .travis/post-build.sh
@@ -7,22 +7,27 @@ latest_images=()
 
 docker_base=${DOCKER_USER}/dnsdock
 
+post_file="/tmp/deploy_containers.sh"
+post(){
+	echo $@ >> $post_file
+}
+
 for a in ${ARCHS[@]}
 do
 	images=("${images[@]}" "${docker_base}:${VERSION}-${a}")
 	latest_images=("${latest_images[@]}" "${docker_base}:latest-${a}")
-	docker tag ${docker_base}:${VERSION}-${a} ${docker_base}:latest-${a}
+	post docker tag ${docker_base}:${VERSION}-${a} ${docker_base}:latest-${a}
+	post docker push ${docker_base}:latest-${a}
 done
 
 docker manifest create ${docker_base}:${VERSION} ${images[@]}
-docker manifest create ${docker_base}:latest ${latest_images[@]}
+post docker manifest create ${docker_base}:latest ${latest_images[@]}
 docker tag ${docker_base}:${VERSION} ${docker_base}:latest
 for a in ${ARCHS[@]}
 do
 	docker manifest annotate ${docker_base}:${VERSION} ${docker_base}:${VERSION}-${a} --arch ${a} --os linux
-	docker manifest annotate ${docker_base}:latest ${docker_base}:latest-${a} --arch ${a}
-	echo "docker push ${docker_base}:latest-${a}" >> /tmp/deploy_containers.sh
+	post docker manifest annotate ${docker_base}:latest ${docker_base}:latest-${a} --arch ${a} --os linux
 done
 
 docker push ${docker_base}:${VERSION}
-echo "docker push ${docker_base}:latest" >> /tmp/deploy_containers.sh
+post docker push ${docker_base}:latest
